f1abe750e50baa85de607cf58fa188b3ddba8e93
Ian Kim <iannsydney@gmail.com>
2025-08-10 14:12:45 -0400

    Made maps work

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import { Dimensions, StyleSheet, View } from 'react-native';
+import * as Location from 'expo-location';
+import * as React from 'react';
+import { Alert, Dimensions, StyleSheet, View } from 'react-native';
 import MapView from 'react-native-maps';
 
 export default function App() {
+  const [location, setLocation] = React.useState<Location.LocationObject | null>(null);
+
+  React.useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('Permission denied', 'Location permission is required to show the map.');
+        return;
+      }
+
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView 
+        style={styles.map}
+        initialRegion={{
+          latitude: location?.coords?.latitude || 37.78825,
+          longitude: location?.coords?.longitude || -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsUserLocation={true}
+      />
     </View>
   );
 }
