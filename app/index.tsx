3705d0f7819214244424bcbe729deaef74facfee
Ian Kim <iannsydney@gmail.com>
2025-08-11 00:27:23 -0400

    Map works, Google Search feature doesn't

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,82 @@
-import * as Location from 'expo-location';
-import * as React from 'react';
-import { Alert, Dimensions, StyleSheet, View } from 'react-native';
-import MapView from 'react-native-maps';
+import React from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import MapView, { Callout, Circle, Marker } from 'react-native-maps';
+
 
 export default function App() {
-  const [location, setLocation] = React.useState<Location.LocationObject | null>(null);
-
-  React.useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        Alert.alert('Permission denied', 'Location permission is required to show the map.');
-        return;
-      }
-
-      let currentLocation = await Location.getCurrentPositionAsync({});
-      setLocation(currentLocation);
-    })();
-  }, []);
+  const [pin,setPin] = React.useState ({
+    latitude: 37.78825,
+    longitude: -122.4324
+  })
+  const [region,setRegion] = React.useState ({
+    latitude: 37.78825,
+    longitude: -122.4324
+  })
 
   return (
-    <View style={styles.container}>
+    <View style={{marginTop: 50, flex: 1}}>
+      {/*<GooglePlacesAutocomplete
+        placeholder="Search"
+        onPress={(data: any, details: any = null) => {
+          console.log(data, details);
+        }}
+        query={{
+          key: 'AIzaSyDG258Q2a5mxCRr940XSjOi3lQxZR-CidU',
+          language: 'en',
+        }}
+        styles={{
+          container: { 
+            position: "absolute", 
+            top: 0,
+            left: 20,
+            right: 20,
+            zIndex: 1000,
+            height: 50
+          },
+          textInput: {
+            height: 50,
+            backgroundColor: 'white',
+            borderRadius: 8,
+            paddingHorizontal: 15,
+            fontSize: 16,
+          }
+        }}
+      />*/}
       <MapView 
         style={styles.map}
         initialRegion={{
-          latitude: location?.coords?.latitude || 37.78825,
-          longitude: location?.coords?.longitude || -122.4324,
+          latitude: 37.78825,
+          longitude: -122.4324,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-        showsUserLocation={true}
-      />
+      >
+        <Marker 
+          coordinate={pin}
+          pinColor = "purple"
+          draggable={true}
+          onDragStart={(e) => {
+            console.log("Drag start", e.nativeEvent.coordinate)
+          }}
+          onDragEnd={(e) => {
+            setPin({
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude
+            })
+          }}
+          
+        >
+          <Callout>
+            <View style={{ padding: 10, backgroundColor: 'white', borderRadius: 5 }}>
+              <Text style={{ fontSize: 16, fontWeight: 'bold' }}>ASDASDASDASDASDASDASDASDASDAS</Text>
+            </View>
+          </Callout>
+        </Marker>
+        <Circle 
+          center={pin} 
+          radius={1000} 
+        />
+      </MapView>
     </View>
   );
 }
